def25a4f879bab72eef7ad085491f7b8f5cd1792
Avinash Kulal <avinash.kulal@juegostudioz.com>
2022-07-12 17:17:13 +0530

    create express images upload docker image

---
+++ ImageUpload/index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const app = express();
+const path = require("path")
+const multer = require("multer")
+require("dotenv-defaults").config();
+const { env } = process;
+
+const storage = multer.diskStorage({
+ destination: function (req, file, cb) {
+  cb(null, path.join(__dirname, '/images'))
+ },
+ filename: function (req, file, cb) {
+  cb(null, 'image.jpg');
+ }
+})
+
+const upload = multer({ storage: storage })
+app.use(express.static('images'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/pages'))
+app.get('/', (req, res) => {
+ return res.render('home', { data: env.PORT });
+})
+
+app.get('/upload', (req, res) => {
+ return res.render('upload');
+})
+
+app.get('/image', (req, res) => {
+ return res.render('image')
+})
+
+app.post('/upload', upload.single('file'),(req, res) => {
+ return res.redirect('/image')
+})
+
+app.listen(env.PORT, _ => {
+ console.log(`App listen on prot ${env.PORT}`)
+})
